10f78363ee11a50f5cb01c725c27c3b982dbe7d9
boura86 <bouradiallo86@gmail.com>
2018-12-09 01:25:41 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'swpa';
+  config: any;
+  fullpage_api: any;
+  constructor() {
+    this.config = {
+          // fullpage options
+          licenseKey: 'YOUR LICENSE KEY HERE',
+          anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'lastPage'],
+          menu: '#menu',
+         // scrollHorizontally:true,
+          // fullpage callbacks
+          afterResize: () => {
+            console.log("After resize");
+          },
+          afterLoad: (origin, destination, direction) => {
+            console.log(origin.index);
+          }
+    };
+  }
+  getRef(fullPageRef) {
+    this.fullpage_api = fullPageRef;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { AngularFullpageModule } from '@fullpage/angular-fullpage';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFullpageModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
