7456461452893e8fee5872d00cb68075a2c596d7
Abdoulaye SAWARE <ds.truelink@gmail.com>
2018-12-12 22:42:21 +0100

    reorganisation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,36 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'swpa';
-  config: any;
-  fullpage_api: any;
+  
   constructor() {
-    this.config = {
-          // fullpage options
-          licenseKey: 'IlsiwsTP1!',
-          sectionsColor: ['#7BAABE', '#4BBFC3', '#7BAABE', '#ccddff', '#7BAABE'],
-          anchors: ['home', 'service', 'blog', 'contact', 'lastPage'],
-          menu: '#menu',
-          scrollOverflow: true,
-      
-         // scrollHorizontally:true,
-          // fullpage callbacks
-        // events callback
-          afterLoad: (origin, destination, direction) => {
-            // console.log(destination);
-          },
-          afterRender: () => {
-            // console.log('afterRender');
-          },
-          afterResize: (width, height) => {
-            // console.log('afterResize' + width + ' ' + height);
-          },
-          afterSlideLoad: (section, origin, destination, direction) => {
-            // console.log(destination);
-          }
-    };
+    
   }
-  getRef(fullPageRef) {
-    console.log('iicii');
-    this.fullpage_api = fullPageRef;
-  }
+ 
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,10 +6,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  config: any;
+  fullpage_api: any;
 
-  constructor() { }
+  constructor() { 
+    this.config = {
+      // fullpage options
+      licenseKey: 'IlsiwsTP1!',
+      sectionsColor: ['#7BAABE', '#4BBFC3', '#7BAABE', '#ccddff', '#7BAABE'],
+      anchors: ['service', 'blog', 'contact'],
+      menu: '#menu',
+      scrollOverflow: true,
+  
+     // scrollHorizontally:true,
+      // fullpage callbacks
+    // events callback
+      afterLoad: (origin, destination, direction) => {
+        // console.log(destination);
+      },
+      afterRender: () => {
+        // console.log('afterRender');
+      },
+      afterResize: (width, height) => {
+        // console.log('afterResize' + width + ' ' + height);
+      },
+      afterSlideLoad: (section, origin, destination, direction) => {
+        // console.log(destination);
+      }
+    };
+    
+  }
 
   ngOnInit() {
+  }
+  getRef(fullPageRef) {
+    console.log('iicii');
+    this.fullpage_api = fullPageRef;
   }
 
 }
